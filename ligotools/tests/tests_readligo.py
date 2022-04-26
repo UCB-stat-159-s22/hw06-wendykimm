9c4004f5cf7533271b12fe54b9be80d7eca1eae6
Wendy Kim <wendykimm@berkeley.edu>
2022-04-26 19:14:17 +0000

    binder

--- ligotools/tests/tests_readligo.py
+++ ligotools/tests/tests_readligo.py
@@ -12,11 +12,6 @@ import json
 import h5py
 import os
 
-def test_loaddata():
-    strain, time, chan_dict = rl.loaddata('data/L-L1_LOSC_4_V2-1126259446-32.hdf5', 'H1')
-    test, gpsStart, ts, qmask, shortnameList, injmask, injnameList = rl.read_frame('data/L-L1_LOSC_4_V2-1126259446-32.hdf5', 'H1', True)
-    assert strain == test
-
 # read_frame(filename, ifo, readstrain=True)
 
 # read_hdf5(filename, readstrain=True)
